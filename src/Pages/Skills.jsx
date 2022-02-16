808418bf679e20f64d2d75fcab7f2a81b4ae58ca
Abdallah Ali <amaali1991@gmail.com>
2022-02-16 15:30:15 +0200

    fix home media quray 19

--- src/Pages/Skills.jsx
+++ src/Pages/Skills.jsx
@@ -3,20 +3,28 @@ import React from "react";
 const Skills = () => {
   return (
     <section id="Skills" className="page-section skills-page">
-      <div className="skills">
+      <div className="conta">
         <h1 className="rounded-header">Skills</h1>
-        <h6 className="skill">C/C++ Programing Languages</h6>
-        <h6 className="skill">Python Programing Languages</h6>
-        <h6 className="skill">JavaScript Programing Languages</h6>
-        <h6 className="skill">Sass Programing Languages</h6>
+        <h6 className="skill">C/C++</h6>
+        <h6 className="skill">Python</h6>
+        <h6 className="skill">JavaScript</h6>
+        <h6 className="skill">Sass</h6>
         <h6 className="skill">Bash Script</h6>
         <h6 className="skill">HTML</h6>
         <h6 className="skill">Tex</h6>
-        <h6 className="skill">FreeRTOS / ESP32 </h6>
+      </div>
+      <div className="conta">
+        <h1 className="rounded-header">Framworks</h1>
         <h6 className="skill">Arduino</h6>
+        <h6 className="skill">ESP32/8266 Arduino</h6>
+        <h6 className="skill">FreeRTOS</h6>
+        <h6 className="skill">Linux For Embedded</h6>
+        <h6 className="skill">Flask</h6>
+        <h6 className="skill">ReactJS</h6>
+        <h6 className="skill">BootStrap</h6>
         <h6 className="skill">Linux OS</h6>
       </div>
-      <div className="soft-skills">
+      <div className="conta">
         <h1 className="rounded-header">Soft Skills</h1>
         <h6 className="skill"> Communication</h6>
         <h6 className="skill">Integrity</h6>
@@ -27,7 +35,7 @@ const Skills = () => {
         <h6 className="skill">Commitment </h6>
         <h6 className="skill">Time Management</h6>
       </div>
-      <div className="lang">
+      <div className="conta">
         <h1 className="rounded-header">Languages</h1>
         <h6 className="skill">Arabic (Mother Tongue) </h6>
         <h6 className="skill">English</h6>
